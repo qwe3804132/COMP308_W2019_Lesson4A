9a95f7f889b54da5f147fe2fc967ae018955c804
qwe3804132 <liupeiran9324@outlook.com>
2019-01-26 15:34:49 -0500

    first

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,26 @@ let router = express.Router();
 
 
 router.get('/', (req, res, next) =>{
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+router.get('/about', (req, res, next) =>{
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contact', (req, res, next) =>{
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/products', (req, res, next) =>{
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', (req, res, next) =>{
+  res.render('index', { title: 'Services' });
+});
+
+
+
 
 module.exports = router;
